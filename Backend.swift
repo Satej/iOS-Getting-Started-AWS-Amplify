ed4d2d6191404ef84ff2425f4a437960d191dbe8
Satej <Satej>
2023-01-04 18:18:55 +0530

    Added user authentication

--- Backend.swift
+++ Backend.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Satej Sahu on 04/01/23.
 //
-
+import AmplifyPlugins
 import UIKit
 import Amplify
 
@@ -16,10 +16,76 @@ class Backend {
     private init() {
       // initialize amplify
       do {
+        try Amplify.add(plugin: AWSCognitoAuthPlugin())
         try Amplify.configure()
+          _ = Amplify.Hub.listen(to: .auth) { (payload) in
+
+              switch payload.eventName {
+
+              case HubPayload.EventName.Auth.signedIn:
+                  print("==HUB== User signed In, update UI")
+                  self.updateUserData(withSignInStatus: true)
+
+              case HubPayload.EventName.Auth.signedOut:
+                  print("==HUB== User signed Out, update UI")
+                  self.updateUserData(withSignInStatus: false)
+
+              case HubPayload.EventName.Auth.sessionExpired:
+                  print("==HUB== Session expired, show sign in UI")
+                  self.updateUserData(withSignInStatus: false)
+
+              default:
+                  //print("==HUB== \(payload)")
+                  break
+              }
+          }
+           
+          // let's check if user is signedIn or not
+           _ = Amplify.Auth.fetchAuthSession { (result) in
+               do {
+                   let session = try result.get()
+                          
+          // let's update UserData and the UI
+               self.updateUserData(withSignInStatus: session.isSignedIn)
+                          
+               } catch {
+                    print("Fetch auth session failed with error - \(error)")
+              }
+          }    
         print("Initialized Amplify");
       } catch {
         print("Could not initialize Amplify: \(error)")
       }
+    }
+    
+    // signin with Cognito web user interface
+    public func signIn() {
+        _ = Amplify.Auth.signInWithWebUI(presentationAnchor: UIApplication.shared.windows.first!) { result in
+            switch result {
+            case .success(_):
+                print("Sign in succeeded")
+            case .failure(let error):
+                print("Sign in failed \(error)")
+            }
+        }
+    }
+    
+    // signout
+    public func signOut() {
+        _ = Amplify.Auth.signOut() { (result) in
+            switch result {
+            case .success:
+                print("Successfully signed out")
+            case .failure(let error):
+                print("Sign out failed with error \(error)")
+            }
+        }
+    }
+    
+    func updateUserData(withSignInStatus status : Bool) {
+        DispatchQueue.main.async() {
+            let userData : UserData = .shared
+            userData.isSignedIn = status
+        }
     }
 }
--- Podfile
+++ Podfile
@@ -9,4 +9,6 @@ target 'iOS Getting Started' do
   pod 'Amplify', '~> 1.0'             # required amplify dependency
   pod 'Amplify/Tools', '~> 1.0'       # allows to call amplify CLI from within Xcode
 
+  pod 'AmplifyPlugins/AWSCognitoAuthPlugin', '~> 1.0' # support for Cognito user authentication
+
 end
