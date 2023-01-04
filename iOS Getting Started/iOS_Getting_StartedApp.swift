8cffd2a70ab66bc31f154c03b0850f0421f238d7
Satej <Satej>
2023-01-04 16:42:30 +0530

    Initial Commit

---
+++ iOS Getting Started/iOS_Getting_StartedApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Getting_StartedApp.swift
+//  iOS Getting Started
+//
+//  Created by Satej Sahu on 04/01/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Getting_StartedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
